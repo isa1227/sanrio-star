b5b0196a5f4c8ee4b88a3f7364e760ddbc303a5f
valcallejas <valery12callejas@gmail.com>
2025-08-03 23:01:03 -0500

    Boton Keroppi

--- frontend/src/pages/Keroppi.jsx
+++ frontend/src/pages/Keroppi.jsx
@@ -151,6 +151,15 @@ export default function Keroppi() {
         <p>Teléfono: +123 456 789</p>
         <p>© 2024 Sanrio Star</p>
       </footer>
+
+      <button
+      className="scroll-top-btn-keroppi"
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
+      🐸
+      </button>
+
+
     </div>
   );
 }
