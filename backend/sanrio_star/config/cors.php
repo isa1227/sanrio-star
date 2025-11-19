dd142900115433252fd4a626770b0199d45891e2
Jouler Talaigua <jouler.talaigua@partners.goodrx.com>
2025-11-19 16:05:02 -0500

    unnecesary code

--- backend/sanrio_star/config/cors.php
+++ backend/sanrio_star/config/cors.php
@@ -9,14 +9,12 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://react-frontend-production-2ec7.up.railway.app',
-        'https://react-frontend-production-2ec7.up.railway.app/',
         env('FRONTEND_URL'),
     ],
 
     'allowed_origins_patterns' => [
         // allow the railway.app frontend (http or https, optional port)
-        '^https?://react-frontend-production-2ec7\.up\.railway\.app(:[0-9]+)?$',
+        //'^https?://react-frontend-production-2ec7\.up\.railway\.app(:[0-9]+)?$',
     ],
 
     'allowed_headers' => ['*'],
